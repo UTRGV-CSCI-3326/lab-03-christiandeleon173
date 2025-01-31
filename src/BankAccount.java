95e9d67284864fdb4a9a39e034b242782c82b984
christiandeleon173 <christiandeleon173@gmail.com>
2025-01-30 19:14:39 -0600

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,17 @@
 
+public class BankAccount {
+    public static void main(String[] args){
+        float bobMoney = 2175.37f;
+
+        bobMoney -= 302.50;
+        bobMoney += 50.03;
+        bobMoney  = (bobMoney/2.f) + 20.f;
+        bobMoney -= 1.f;
+        bobMoney *= 2.f;
+        bobMoney += 1.f;
+
+        bobMoney = Math.round(bobMoney * 100.f) / 100.f;
+
+        System.out.printf("%.2f\n", bobMoney);
+    }
+}
